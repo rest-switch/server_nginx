a6641179f16f77db60c88848e4633e49a93bb4c7
John Clark <inindev@gmail.com>
2015-08-23 10:52:53 -0400

    take userid and password from the command line

--- docker/make_pass.sh
+++ docker/make_pass.sh
@@ -1,14 +1,41 @@
 #!/bin/sh -e
+#
+# Copyright 2015 The REST Switch Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this PASS_FILE except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, Licensor provides the Work (and each Contributor provides its 
+# Contributions) on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including, 
+# without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE. You are solely responsible for determining the appropriateness of using or redistributing the Work and assume any 
+# risks associated with Your exercise of permissions under this License.
+#
+# Author: John Clark (johnc@restswitch.com)
+#
 
-USER="myuser"
-PASS="mypass"
-FILE="passwd"
 
-CRYPT=$(openssl passwd "$PASS")
+USERID="$1"
+PASSWD="$2"
+if [ -z $USERID ] || [ -z $PASSWD ]; then
+    echo
+    echo "usage: $0 <userid> <password>"
+    echo
+    exit 1
+fi
 
-rm -rf "${FILE}"
-touch "${FILE}"
-chown root:nginx "${FILE}"
-chmod 640 "${FILE}"
-echo "$USER:$CRYPT" > "${FILE}"
+MYFILE=$(readlink -f "$0")
+MYDIR=$(dirname "${MYFILE}")
+PASS_FILE="${MYDIR}/passwd"
+
+CRYPT=$(openssl passwd "$PASSWD")
+
+rm -rf "${PASS_FILE}"
+touch "${PASS_FILE}"
+chown root:nginx "${PASS_FILE}"
+chmod 640 "${PASS_FILE}"
+echo "$USERID:$CRYPT" > "${PASS_FILE}"
 
